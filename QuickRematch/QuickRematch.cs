7cc050b7ed2963d33846593969793ed8961af399
Glomzubuk <glomzubuk@gmail.com>
2025-02-17 12:39:33 +0100

    Added compatibility with Texmod 2.3.0 and up

--- QuickRematch/QuickRematch.cs
+++ QuickRematch/QuickRematch.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Reflection;
+using System.Text;
 using UnityEngine;
 using HarmonyLib;
 using BepInEx;
@@ -9,17 +11,25 @@ using LLBML;
 using LLBML.States;
 using LLBML.Players;
 using LLBML.GameEvents;
+using LLBML.Utils;
 
 namespace QuickRematch
 {
     [BepInPlugin(PluginInfos.PLUGIN_ID, PluginInfos.PLUGIN_NAME, PluginInfos.PLUGIN_VERSION)]
     [BepInDependency(LLBML.PluginInfos.PLUGIN_ID, BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency(TEXMOD_GUID, BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("no.mrgentle.plugins.llb.modmenu", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInProcess("LLBlaze.exe")]
     class QuickRematch : BaseUnityPlugin
     {
         public static ManualLogSource Log { get; private set; } = null;
         public static QuickRematch Instance { get; private set; } = null;
+
+        public const string TEXMOD_GUID = "no.mrgentle.plugins.llb.texturemod";
+        public static BaseUnityPlugin texmodInstance = null;
+        public static MethodInfo TM_getSkinMethod = null;
+        public static MethodInfo TM_setSkinMethod = null;
+
         public static ConfigEntry<bool> autoRematch;
         public static ConfigEntry<bool> autoSelectCharacter;
         public static ConfigEntry<bool> autoReready;
@@ -27,9 +37,11 @@ namespace QuickRematch
         public static ConfigEntry<bool> storeCharacterOnRelaunch;
         public static ConfigEntry<int> storedCharacter;
         public static ConfigEntry<int> storedVariant;
+        public static ConfigEntry<string> storedSkin;
 
         public static Character characterSelected = Character.NONE;
         public static CharacterVariant variantSelected = CharacterVariant.DEFAULT;
+        public static byte[] skinSelected = null;
 
         void Awake()
         {
@@ -52,6 +64,7 @@ namespace QuickRematch
             {
                 characterSelected = (Character)storedCharacter.Value;
                 variantSelected = (CharacterVariant)storedVariant.Value;
+                skinSelected = Convert.FromBase64String(storedSkin.Value);
             }
 
             LLBML.Utils.ModDependenciesUtils.RegisterToModMenu(this.Info);
@@ -61,6 +74,17 @@ namespace QuickRematch
             };
             LobbyEvents.OnUserCharacterPick += OnUserCharacterPick;
             LobbyEvents.OnUserSkinClick += OnUserSkinClick;
+
+            if (ModDependenciesUtils.IsModLoaded(TEXMOD_GUID))
+            {
+                var TMInfos = BepInEx.Bootstrap.Chainloader.PluginInfos[TEXMOD_GUID];
+                if (TMInfos.Metadata.Version.Major >= 2 && TMInfos.Metadata.Version.Minor >= 3)
+                {
+                    texmodInstance = BepInEx.Bootstrap.Chainloader.PluginInfos[TEXMOD_GUID].Instance;
+                    TM_getSkinMethod = AccessTools.Method(texmodInstance.GetType(), "External_GetSkinHashFromPlayer", new [] { typeof(int) });
+                    TM_setSkinMethod = AccessTools.Method(texmodInstance.GetType(), "External_AssignSkinToPlayer", new[] { typeof(int), typeof(byte[]) });
+                }
+            }
         }
 
         void InitModOptions(ConfigFile config)
@@ -72,6 +96,7 @@ namespace QuickRematch
             storeCharacterOnRelaunch = config.Bind<bool>("Toggles", "storeCharacterOnRelaunch", false);
             storedCharacter = config.Bind<int>("Storage", "storedCharacter", (int)Character.NONE, new ConfigDescription("", null, "modmenu_hidden"));
             storedVariant = config.Bind<int>("Storage", "storedVariant", (int)CharacterVariant.DEFAULT, new ConfigDescription("", null, "modmenu_hidden"));
+            storedSkin = config.Bind<string>("Storage", "storedSkin", "", new ConfigDescription("", null, "modmenu_hidden"));
         }
 
         private void OnUserCharacterPick(PlayersCharacterButton pcb, OnUserCharacterPickArgs args)
@@ -120,11 +145,25 @@ namespace QuickRematch
         {
             characterSelected = character;
             variantSelected = variant;
+
+            if (texmodInstance != null)
+            {
+                try
+                {
+                    skinSelected = TM_getSkinMethod.Invoke(texmodInstance, new object[] { Player.GetLocalPlayer().nr }) as byte[];
+                }
+                catch (Exception e)
+                {
+                    Log.LogError("Caught exception trying to get the skin of the current player: " + e);
+                }
+            }
+
             Log.LogInfo("Saving char: " + QuickRematch.characterSelected + " | " + QuickRematch.variantSelected);
             if (storeCharacterOnRelaunch.Value)
             {
                 storedCharacter.Value = (int)characterSelected;
                 storedVariant.Value = (int)variantSelected;
+                storedSkin.Value = Convert.ToBase64String(skinSelected ?? new byte[0]);
             }
         }
 
@@ -142,6 +181,17 @@ namespace QuickRematch
                 player.CharacterVariant = variantSelected;
             }
 
+            if (texmodInstance != null && skinSelected?.Length > 0 )
+            {
+                try
+                {
+                    TM_setSkinMethod.Invoke(texmodInstance, new object[] { Player.GetLocalPlayer().nr, skinSelected });
+                }
+                catch (Exception e)
+                {
+                    Log.LogError("Caught exception trying to set the skin of the current player: " + e);
+                }
+            }
             GameStatesLobbyUtils.RefreshLocalPlayerState();
         }
 
